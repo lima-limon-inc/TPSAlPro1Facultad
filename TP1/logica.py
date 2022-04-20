ce13230c2190df611445913cfa8ac9c15bab5e04
Fabri <moboficial00@gmail.com>
2022-04-19 21:38:06 -0300

    Logre crear la logica para mover el vacio. Falta la interaccion del usuario

--- TP1/logica.py
+++ TP1/logica.py
@@ -2,9 +2,9 @@
 from random import randint
 
 # Constantes globales
-MATRIZ_COLUMNAS = 2
-MATRIZ_FILAS    = 2
-
+MATRIZ_COLUMNAS = 4
+MATRIZ_FILAS    = 4
+ESPACIO_VACIO   = "E"
 
 # Funciones
 def generar_cantidad_cuadrados():
@@ -17,7 +17,7 @@ def generar_cantidad_cuadrados():
 
     for i in range(1,cantidad_de_cuadrados): #Arranca desde 1 porque el 0 no es usado en el juego
         lista_con_cuadrados.append(i)
-    lista_con_cuadrados.append("")
+    lista_con_cuadrados.append(ESPACIO_VACIO)
 
     return lista_con_cuadrados
     
@@ -41,11 +41,60 @@ def generar_matriz():
 
         for cuadrado in range(MATRIZ_COLUMNAS):
             cuadrados.pop(0)
+    
+    return matriz
 
-        return matriz
+def aleatorizar_matriz(movimientos): #Movimientos es una lista
+    '''
+    Funcion que, dada una matriz, aleatoriza todas las posiciones
+    '''
+
+    matriz = generar_matriz() #TODO: Cambiar nombre
+    for i in range(len(matriz)):
+        print(matriz[i])  
+    
+    print()
+    coord_vacio = [MATRIZ_FILAS - 1, MATRIZ_COLUMNAS - 1] #TODO: BORRAR coord_vacio[0] = Fila; #coord_vacio[1] = Columna
+    print(coord_vacio)
+    print()
+
+
+    for movimiento in movimientos:
+        if movimiento == "Abajo": 
+            #Ejecuta lo de abajo si y solo si el vacio no esta arriba de todo (esto mismo aplica para los otros 4)
+            if coord_vacio[0] != 0: 
+                #Coordenada del vacio en matriz        Coordenada de misma columna, 1 fila arriba   
+                matriz[coord_vacio[0]][coord_vacio[1]],matriz[coord_vacio[0] - 1][coord_vacio[1]] = matriz[coord_vacio[0] - 1][coord_vacio[1]],matriz[coord_vacio[0]][coord_vacio[1]]
+                coord_vacio[0] = coord_vacio[0] - 1
+
+        if movimiento == "Derecha":
+            if coord_vacio[1] != 0:
+                #Coordenada del vacio en matriz        Coordenada de misma columna, 1 column  izq
+                matriz[coord_vacio[0]][coord_vacio[1]],matriz[coord_vacio[0]][coord_vacio[1] - 1] = matriz[coord_vacio[0]][coord_vacio[1] - 1],matriz[coord_vacio[0]][coord_vacio[1]]
+                coord_vacio[1] = coord_vacio[1] - 1
+
+        if movimiento == "Arriba": 
+            if coord_vacio[0] != 0: 
+                #Coordenada del vacio en matriz        Coordenada de misma columna, 1 fila abajo   
+                matriz[coord_vacio[0]][coord_vacio[1]],matriz[coord_vacio[0] + 1][coord_vacio[1]] = matriz[coord_vacio[0] + 1][coord_vacio[1]],matriz[coord_vacio[0]][coord_vacio[1]]
+                coord_vacio[0] = coord_vacio[0] + 1
+
+        if movimiento == "Izquierda":
+            if coord_vacio[1] != MATRIZ_COLUMNAS - 1:
+                #Coordenada del vacio en matriz        Coordenada de misma columna, 1 column der
+                matriz[coord_vacio[0]][coord_vacio[1]],matriz[coord_vacio[0]][coord_vacio[1] + 1] = matriz[coord_vacio[0]][coord_vacio[1] + 1],matriz[coord_vacio[0]][coord_vacio[1]]
+                coord_vacio[1] = coord_vacio[1] + 1
+       
+
+    print()
+    print(coord_vacio)
+    print()
+
+    for i in range(len(matriz)):
+        print(matriz[i])     
 
 # Funciones debug BORRAR DESPUES
-print(generar_matriz())
+aleatorizar_matriz(["Abajo","Derecha","Derecha","Abajo","Izquierda","Arriba","Derecha"])
 #print(generar_cantidad_cuadrados())
 
 # Funcion para que queda linda la matriz, anadir ljust y rjust
