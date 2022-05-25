353374343fd7b24d5857fdc463e18d6309a9777d
Fabri <moboficial00@gmail.com>
2022-05-25 11:00:58 -0300

    Anadi el procesamiento de archivos

--- TP2/logica.py
+++ TP2/logica.py
@@ -1,9 +1,21 @@
-
 # Constantes relacionadas con el tablero:
 FILAS = 8
 COLUMNAS = 8
 ULTIMA_FILA = FILAS - 1
 ULTIMA_COLUMNA = COLUMNAS - 1
+
+def leer_movimientos(archivo):
+    diccionario_movimientos = {}
+    with open(archivo, "r") as f:
+        for linea in f:
+            pieza, dir_x_dir_y, extensible = linea.rstrip("\n").split(",")
+            diccionario_movimientos[pieza] = diccionario_movimientos.get(pieza, {"extensible":extensible, "movimientos":[]}) #"Sobreescribe" el valor si ya existe consigo mismo, caso contrario crea una lista vacia como valor
+
+            diccionario_movimientos[pieza]["movimientos"].append((dir_x_dir_y.split(";")))
+
+    return diccionario_movimientos
+
+print(leer_movimientos("movimientos.csv"))
 
 class Tablero:
     def __init__(self):
@@ -20,4 +32,12 @@ class Tablero:
         
         return self.tablero_mutable
 
-    
+    def casillas_ocupadas(self):
+        return self.tablero_mutable.keys()
+
+class Caballo:
+    pass
+class Alfil:
+    pass
+class Torre:
+    pass
