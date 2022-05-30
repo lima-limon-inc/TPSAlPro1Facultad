775dea5a10b69d8700fee65aba2a57da0bd97be9
Fabri <moboficial00@gmail.com>
2022-05-30 16:39:59 -0300

    Cambios generales a la estructura del programa

--- TP2/logica.py
+++ TP2/logica.py
@@ -25,66 +25,83 @@ COLOR_NEGRO  = "#181818"
 
 
 
-class Tablero:
-    def __init__(self):
-        self.tablero = {} # (Columna (x), Fila (y)): Pieza
-        self.tablero_mutable = {}
-        self.nivel = 1 #Primer nivel
+class Game:
+    def __init__(self, nivel):
+        self.nivel = nivel
+        self.tablero = Tablero(self.nivel)
 
+    def siguienteNivel(self):
+        self.nivel += 1
+        self.tablero = Tablero(self.nivel)
+
+    def procesar_clicks(self, coord_x, coord_y):
+        for columna in range(COLUMNAS): #Funcion que transforma las coordenadas x e y del mouse en coordenadas de la lista. Ejemplo:  (x: 113, y:237) --> (x: 3, y:7)
+            if not (columna * CUADRADO_ANCHO <= x <= columna * CUADRADO_ANCHO + CUADRADO_ANCHO): 
+                continue
+            x = columna
+            for fila in range(FILAS):
+                if not (fila * CUADRADO_ALTO <= y <= fila * CUADRADO_ALTO + CUADRADO_ALTO):
+                    continue
+                y = fila
+
+
+
+class Tablero:
+    def __init__(self, nivel):
+        self.tablero = {} # (Columna (x), Fila (y)): Pieza
         # El siguiente codigo se encarga de generar el tablero
         #  ----------------------------------------------
 
         # La primera ficha es elegida aleatoriamente
-        columna = randint(0,7)
-        fila = randint(0,7)
-        self.tablero[columna, fila] = Pieza(choice(list(MOVIMIENTOS.keys())), True) #MOVIMIENTOS.keys() son todos los tipos de piezas que el programa leyo en el archivo movimientos.csv
+        columna, fila = randint(0,7), randint(0,7)
+
+        self.tablero[columna, fila] = Pieza(columna, fila, choice(list(MOVIMIENTOS.keys())), True, self.casillas_ocupadas()) #MOVIMIENTOS.keys() son todos los tipos de piezas que el programa leyo en el archivo movimientos.csv. Empieza en True, porque la primera pieza generada es con la que el jugador empieza
 
         self.pieza_seleccionada = (columna, fila) #Hace referencia a la ficha con la que el jugador empieza
 
-        for i in range(1, self.nivel + 2): #El "1," se debe a que la primera pieza la genero fuera del for loop
-            columna, fila = choice(list(self.tablero[columna, fila].calcular_movimientos_validos(columna, fila, self.casillas_ocupadas())))
+        for i in range(1, nivel + 2): #El "1," se debe a que la primera pieza la genero fuera del for loop
+            columna, fila = choice(list(self.tablero[columna, fila].movimientos_validos)) #Elige una posicion dentro de las posiciones validas de la ultima pieza, para poner la nueva pieza
 
-            self.tablero[columna, fila] = Pieza(choice(list(MOVIMIENTOS.keys())), False) #MOVIMIENTOS.keys() son todos los tipos de piezas
+            self.tablero[columna, fila] = Pieza(columna, fila, choice(list(MOVIMIENTOS.keys())), False, self.casillas_ocupadas()) #MOVIMIENTOS.keys() son todos los tipos de piezas que el programa leyo en el archivo movimientos.csv. Empieza en True, porque la primera pieza generada es con la que el jugador empieza
 
-        self.tablero_mutable = dict(self.tablero) #El tablero mutable es donde el usuario interactua, el tablero no muta (se usa como "failsafe" por si el jugador se queda trabado
 
-    def actualizar_tablero(self, columna_movida, fila_movida):
+        self.failsafe = (dir(self.tablero), tuple(self.pieza_seleccionada))
+
+    def actualizar_tablero(self, columna_destino, fila_destino):
         """Funcion que toma una ficha de la posicion (x1,y1) y la lleva a la posicion (x2,y2)"""
-        if columna_movida > ULTIMA_COLUMNA or columna_movida < 0:
+        if columna_destino > ULTIMA_COLUMNA or columna_destino < 0:
             raise IndexError(f"Error, {columna} no es una columna valida, el tablero es de {FILAS} x {COLUMNAS}; y la ultima columna valida es {ULTIMA_COLUMNA}")
-        elif fila_movida > ULTIMA_FILA or fila_movida < 0:
+        elif fila_destino > ULTIMA_FILA or fila_destino < 0:
             raise IndexError(f"Error, {fila} no es una columna valida, el tablero es de {FILAS} x {COLUMNAS}; y la ultima fila valida es {ULTIMA_FILA}") #Estos dos errores no deberian ocurrir, pero si llegan a ocurrir, se tiene que crashear el programa para evitar un comportamiento no deseado
+
+        if not self.tablero_mutable[columna_destino, fila_destino].seleccionado:
+            self.tablero_mutable[columna_destino, fila_destino].hacer_activa()
+
         self.tablero_mutable.pop(self.pieza_seleccionada)
 
-        self.pieza_seleccionada = (columna_movida, fila_movida)
+        self.pieza_seleccionada = (columna_destino, fila_destino)
 
     def casillas_ocupadas(self):
         return self.tablero.keys()
 
-    def generar_tablero(self):
-        columna = randint(0,7)
-        fila = randint(0,7)
-        self.tablero[columna, fila] = Pieza(choice(list(MOVIMIENTOS.keys())), True) #MOVIMIENTOS.keys() son todos los tipos de piezas que el programa leyo en el archivo movimientos.csv
-        self.pieza_seleccionada = (columna, fila)
-
-        #print(f"DEBUG Aleatorio: {columna} {fila}")
-        for i in range(1, self.nivel + 2): #El "1," se debe a que la primera pieza la genero fuera del for loop
-            columna, fila = choice(list(self.tablero[columna, fila].calcular_movimientos_validos(columna, fila, self.casillas_ocupadas())))
-
-            self.tablero[columna, fila] = Pieza(choice(list(MOVIMIENTOS.keys())), False) #MOVIMIENTOS.keys() son todos los tipos de piezas
-
-        self.tablero_mutable = dict(self.tablero) #El tablero mutable es donde el usuario interactua, el tablero no muta (se usa como "failsafe" por si el jugador se queda trabado
-
 class Pieza:
-    def __init__(self, tipo, seleccionado):
-        self.tipo = tipo
-        self.seleccionado = seleccionado #Booleano
-        self.imagen = self.devolver_imagen(self.seleccionado)
-        self.movimientos_validos = set()  #TODO: Posiblemente borrar
-        print(f"DEBUG {self.tipo} CREADO. Movimientos {self.movimientos_validos}")
+    def __init__(self,columna, fila, tipo, seleccionado, casillas_ocupadas):
+        self.fila = fila
+        self.columna = columna
+
+        self.tipo = tipo #Hace referencia a que tipo de pieza es (alfil, caballo, etc)
+
+        self.seleccionado = seleccionado #Booleano, representa si la pieza esta seleccionada. En todo momento deberia haber solo 1 pieza seleccionada
+
+        self.imagen = self.devolver_imagen(self.seleccionado) # La funcion devuelve la imagen, dependiendo de si la ficha fue seleccionada o no
+
+        self.movimientos_validos = self.calcular_movimientos_validos(fila, columna, casillas_ocupadas) #
 
     def __str__(self):
         return f"{self.tipo}"
+
+    def hacer_activa(self):
+        self.seleccionado = True
 
     def devolver_imagen(self, seleccionado):
         if seleccionado:
@@ -94,13 +111,13 @@ class Pieza:
 
         return DIRECTORIO_SPRITES + str(self) + color
 
-    def calcular_movimientos_validos(self, columna, fila, casillas_ocupadas): #, casillas, movimientoJugador): #Esta funcion usa como referencia la constante global MOVIMIENTOS. La guarde como constante global ya que es la misma para todas las fichas
+    def calcular_movimientos_validos(self, fila, columna, casillas_ocupadas): #Esta funcion usa como referencia la constante global MOVIMIENTOS. La guarde como constante global ya que es la misma para todas las fichas
         movimientos_validos = set()
         for movimiento in MOVIMIENTOS[str(self)]:
             posibleColumna = columna + movimiento[0]
             posibleFila = fila + movimiento[1]
 
-            if (posibleColumna > 7 or posibleColumna < 0) or (posibleFila > 7 or posibleFila < 0):
+            if (posibleColumna > ULTIMA_COLUMNA or posibleColumna < 0) or (posibleFila > ULTIMA_FILA or posibleFila < 0): #Si se cumple esta condicion, significa que dicha posicion esta fuera de rango del tablero
                 continue
 
             posibleMovimiento = (posibleColumna, posibleFila)
@@ -108,8 +125,7 @@ class Pieza:
             if posibleMovimiento in casillas_ocupadas: #Chequea que no haya dos piezas en el mismo lugar
                 continue
 
-
             movimientos_validos.add(posibleMovimiento)
 
-        self.movimientos_validos = movimientos_validos #TODO: Posiblemente arreglar
         return movimientos_validos
+
